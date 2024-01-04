e8b182441984b0a42187ad7dbb6d76591eabb77a
song <songnumber1@naver.com>
2024-01-04 23:27:34 +0900

    input값 변경되지 않을 경우 props watch 적용 안되는 예제

--- src/components/PropsComponent2.vue
+++ src/components/PropsComponent2.vue
@@ -4,31 +4,31 @@
 
 <script>
 export default {
-  // props: ["params"],
+  props: ["params"],
   mounted() {
-    //console.log("component2", this.params);
+    console.log("component2", this.params);
   },
-  // watch: {
-  //   params: {
-  //     deep: true,
-  //     handler() {
-  //       console.log("변경 감지", this.params);
-  //     },
-  //   },
-  // },
-
-  data() {
-    return {
-      params: {},
-    };
-  },
-
-  methods: {
-    list(params) {
-      this.params = params;
-      console.log("list", this.params);
+  watch: {
+    params: {
+      deep: true,
+      handler() {
+        console.log("변경 감지", this.params);
+      },
     },
   },
+
+  // data() {
+  //   return {
+  //     params: {},
+  //   };
+  // },
+
+  // methods: {
+  //   list(params) {
+  //     this.params = params;
+  //     console.log("list", this.params);
+  //   },
+  // },
 };
 </script>
 
