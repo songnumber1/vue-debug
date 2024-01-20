47e714d9387a181934def988a2da1773f2797ae6
song <songnumber1@naver.com>
2024-01-20 19:24:10 +0900

    JEST node 16.15.0 실행

---
+++ src/components/Jest/Jest.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="background-color: red">
+    <p ref="tgmp"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  mounted() {
+    this.voidRefsFunc();
+  },
+  methods: {
+    voidRefsFunc() {
+      this.$refs.tgmp.innerText = "1234";
+    },
+    returnIntRefsFunc(data) {
+      this.$refs.tgmp.innerText = "1234";
+      return data;
+    },
+    returnIntFunc(data) {
+      return data;
+    },
+  },
+};
+</script>
